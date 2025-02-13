1e08333adc4270f4174f04f5a1b74ec5cea2ea54
lucasnye <141851379+lucasnye@users.noreply.github.com>
2025-02-13 16:40:20 +0800

    test

--- digitRecogniser YOLOv8/main.py
+++ digitRecogniser YOLOv8/main.py
@@ -5,7 +5,7 @@ import cv2
 
 VIDEOS_DIR = os.path.join('.', 'videos')
 
-video_path = os.path.join(VIDEOS_DIR, 'alpaca1.mp4')
+video_path = os.path.join(VIDEOS_DIR, 'IMG_0862.mov')
 video_path_out = '{}_out.mp4'.format(video_path)
 
 cap = cv2.VideoCapture(video_path)
@@ -13,7 +13,7 @@ ret, frame = cap.read()
 H, W, _ = frame.shape
 out = cv2.VideoWriter(video_path_out, cv2.VideoWriter_fourcc(*'MP4V'), int(cap.get(cv2.CAP_PROP_FPS)), (W, H))
 
-model_path = os.path.join('.', 'runs', 'detect', 'train', 'weights', 'last.pt')
+model_path = os.path.join('.', 'runs', 'detect', 'train2', 'weights', 'last.pt')
 
 # Load a model
 model = YOLO(model_path)  # load a custom model
--- digitRecogniser YOLOv8/train.py
+++ digitRecogniser YOLOv8/train.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.yaml")  # build a new model from scratch
 
 # Use the model
-results = model.train(data="data.yaml", epochs=3)  # train the model
+results = model.train(data="data.yaml", epochs=5)  # train the model
